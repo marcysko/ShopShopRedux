773d975a0854288f5f1a4967bb4bbbd56517ec99
Marcia Skousen <75454722+marcysko@users.noreply.github.com>
2021-06-28 11:35:23 -0700

    update app file to use react redux

--- client/src/App.js
+++ client/src/App.js
@@ -14,9 +14,13 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
-import { StoreProvider } from "./utils/GlobalState";
+
 import Success from "./pages/Success";
 import OrderHistory from "./pages/OrderHistory";
+
+import { Provider } from 'react-redux';
+import store from './utils/store';
+
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -42,7 +46,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider>
+        <Provider store={store}>
             <Nav />
             <Switch>
               <Route exact path="/" component={Home} />
@@ -53,7 +57,7 @@ function App() {
               <Route exact path="/products/:id" component={Detail} />
               <Route component={NoMatch} />
             </Switch>
-          </StoreProvider>
+          </Provider>
         </div>
       </Router>
     </ApolloProvider>
